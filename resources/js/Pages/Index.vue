8c79b9e97e846ac0aece49d7c9cd0139bfab9ba5
NaufalikhoAbyan <apradana448@gmail.com>
2024-06-09 19:54:12 +0700

    feat: dashboard added

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,16 +1,33 @@
 <template>
-    <p class="text-3xl">Count: {{ count }}</p>
-    <button @click="increment" class="border rounded-lg bg-blue-500 text-white p-2 active:bg-blue-700">
-        Increment
-    </button>
+    <p class="text-[28px] opacity-60">Dashboard</p>
+    <div class="flex gap-16 px-16 py-4">
+        <div class="card flex gap-8 flex-grow justify-between">
+            <div>
+                <p class="text-primary font-bold">Kategori</p>
+                <p class="text-3xl">0</p>
+            </div>
+            <img src="/images/kategori_icon_black.svg" alt="kategori icon" class="opacity-25 w-8">
+        </div>
+        <div class="card flex gap-8 flex-grow justify-between">
+            <div>
+                <p class="text-green-500 font-bold">Barang</p>
+                <p class="text-3xl">0</p>
+            </div>
+            <img src="/images/barang_icon_black.svg" alt="barang icon" class="opacity-25 w-8">
+        </div>
+        <div class="card flex gap-8 flex-grow justify-between">
+            <div>
+                <p class="text-yellow-500 font-bold">Barang Masuk</p>
+                <p class="text-3xl">0</p>
+            </div>
+            <img src="/images/barang_masuk_icon_black.svg" alt="barang masuk icon" class="opacity-25 w-8">
+        </div>
+        <div class="card flex gap-8 flex-grow justify-between">
+            <div>
+                <p class="text-cyan-500 font-bold">Barang Keluar</p>
+                <p class="text-3xl">0</p>
+            </div>
+            <img src="/images/barang_keluar_icon_black.svg" alt="barang keluar icon" class="opacity-25 w-8">
+        </div>
+    </div>
 </template>
-
-<script setup>
-import { ref } from 'vue'
-
-const count = ref(0)
-
-function increment() {
-    count.value++
-}
-</script>
