19475ef5657c15f4050b2179bfdff1d29cfa7809
NaufalikhoAbyan <apradana448@gmail.com>
2024-06-09 11:08:07 +0700

    Set up VILT stack

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <p class="text-3xl">Count: {{ count }}</p>
+    <button @click="increment" class="border rounded-lg bg-blue-500 text-white p-2 active:bg-blue-700">
+        Increment
+    </button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+    count.value++
+}
+</script>
